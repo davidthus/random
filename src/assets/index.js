5a2a784b2330bab193563d8b2bad474916d04da5
davidthus <dvdnicorici05@gmail.com>
2022-06-11 18:08:55 +0100

    added some songs

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,6 +1,18 @@
 import october from "./october.mp3";
-import escapeplan from "./escapeplan.mp3"
+import flowers from "./flowers.mp3"
 import thrulatenight from "./thrulatenight.mp3";
+import microphonemaster from "./microphonemaster.mp3";
+import sick from "./sick.mp3";
+import sleepyhallow from "./sleepyhallow.mp3";
+import strong from "./strong.mp3";
+import thebiggerpicture from "./thebiggerpicture.mp3";
+import tellem from "./tellem.mp3";
+import hellonearth from "./hellonearth.mp3";
+import kairosdrumming from "./kairosdrumming.mp3";
+import fallingdown from "./fallingdown.mp3";
+import abovetheclouds from "./abovetheclouds.mp3"
+import booksofwar from "./booksofwar.mp3";
+import tonka2 from "./tonka2.mp3"
 
 const songlist = [
   {
@@ -8,13 +20,61 @@ const songlist = [
     src: october,
   },
   {
-    title: "ESCAPE PLAN",
-    src: escapeplan
+    title: "Tonka 2",
+    src: tonka2
+  },
+  {
+    title: "Books of War",
+    src: booksofwar
+  },
+  {
+    title: "Flowers",
+    src: flowers
   },
   {
     title: "Through the late night",
     src: thrulatenight
-  }
+  },
+  {
+    title: "Microphone Master",
+    src: microphonemaster
+  },
+  {
+    title: "Sick",
+    src: sick
+  },
+  {
+    title: "Above The Clouds",
+    src: abovetheclouds
+  },
+  {
+    title: "2055",
+    src: sleepyhallow
+  },
+  {
+    title: "Strong",
+    src: strong
+  },
+  {
+    title: "The Bigger Picture",
+    src: thebiggerpicture
+  },
+  {
+    title: "Tell em",
+    src: tellem
+  },
+  {
+    title: "Hell On Earth",
+    src: hellonearth
+  },
+  {
+    title: "Falling Down",
+    src: fallingdown
+  },
+  {
+    title: "Kairos Drumming Interlude",
+    src: kairosdrumming
+  },
 ];
 
 export default songlist;
