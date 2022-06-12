1219c38ccb8b9f91042af4907f3b29a0e5be1ae6
davidthus <dvdnicorici05@gmail.com>
2022-06-12 11:58:16 +0100

    style header and background gradient

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import './App.css';
-import Header from './components/Header';
-import SongList from './components/SongList';
+import "./App.css";
+import Header from "./components/Header";
+import SongList from "./components/SongList";
 
 function App() {
   return (
-    <div className="App h-screen bg-gradient-to-b from-indigo-500 to-slate-900 p-2">
+    <div className="App overflow-y-auto overscroll-contain p-2">
+      <div className="-z-40 fixed inset-0 h-screen w-screen bg-gradient-to-b from-indigo-500 to-slate-900 bg-repeat-y"></div>
       <Header />
       <SongList />
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Header() {
   return (
     <header>
-      <h1 className="text-white smm:text-4xl sm:text-4xl text-6xl font-black my-3">Random songs for the boys</h1>
+      <h1 className="bg-clip-text text-transparent bg-gradient-to-r from-green-500 to-yellow-500 smm:text-4xl sm:text-4xl text-6xl font-black leading-tight mb-3">Random songs for the boys</h1>
     </header>
   )
 }
